492abbfbaeba718646d428a79f4c797508259551
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-07-02 20:48:21 -0700

    updating routes in thoughts

--- controllers/thoughtControler.js
+++ controllers/thoughtControler.js
@@ -16,6 +16,42 @@ const thoughtController = {
     },
 
     // GET thought by ID
+    getThoughtById( { params }, res) {
+        Thoughts.findOne({ _id: params.id })
+        .populate({
+            path: "reactions",
+            select: "-__v"
+        })
+        .select("-__v")
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json("Error, no thought associated with this ID");
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // Create (PUT) Thought
+    createThought({ params, body }, res) {
+        Thoughts.create(body)
+        .then(({ _id }) => {
+            return Users.findOneAndUpdate(
+                { _id: params.userId }, { $push: { thoughts: _id }}, { new: true, runValidators: true }
+            );
+        })
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json("Error, thought not created");
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.json(err));
+    },
 };
 
 module.exports = thoughtController;
