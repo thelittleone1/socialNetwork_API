a17ec6bb81a995646935869646a592937eda9e45
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-07-02 21:17:18 -0700

    completed routes for thoughts gonna test now

--- controllers/thoughtControler.js
+++ controllers/thoughtControler.js
@@ -71,8 +71,60 @@ const thoughtController = {
         })
         .catch(err => res.status(400).json(err));
     },
+
+    // DELETE Thoughts
+    deleteThought({ params }, res) {
+        Thoughts.findOneAndUpdate({ _id: params.id })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json("Error, thought not found");
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // CREATE reactions
+    addReaction({ params: body}, res) {
+        Thoughts.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body }}, 
+            { new: true, runValidators: true }
+        )
+        .populate({
+            path: "reactions",
+            select: "-__v"
+        })
+        .select("-__v")
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json("Error, thought not found");
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // DELETE Reaction
+    deleteReaction({ params }, res) {
+        Thoughts.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId }}},
+            { new: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json("Error, reaction not found");
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+
 };
-
-
 
 module.exports = thoughtController;
