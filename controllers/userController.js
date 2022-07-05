339d7ec454b96970c67bc6f084d5cddaf57380ea
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-07-04 21:40:18 -0700

    creating user controllers

---
+++ controllers/userController.js
@@ -0,0 +1,41 @@
+// Require User Model
+const { Users } = require("../models");
+
+// Controllers for Users
+const userController = {
+
+    // Create a User
+    createUser( {body}, res) {
+        Users.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // GET all Users
+    getAllUsers(req, res) {
+        Users.find({})
+        // Grab User thoughts
+        .populate({ path: "thoughts", select: "-__v" })
+        // Grab User Friends
+        .populate({ path: "friends", select: "-__v" })
+        .select("-__v")
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // GET User by ID
+    getUserById({params}, res) {
+        Users.findOne({ _id: params.id })
+        .populate({ path: "thoughts", select: "-__v" })
+        .populate({ path: "friends", select: "-__v" })
+        .select("-__v")
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(400).json("No User found with that ID!");
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+};
